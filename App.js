fa76922541765fd3917e240789b0f496ae446a8b
Andrew Gray <andrew.aj.gray@gmail.com>
2021-02-28 00:29:12 +1030

    added all current views, navigation and redux providers to app.js

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./redux/reducers";
+import thunk from "redux-thunk";
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,7 +24,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import LandingScreen from "./components/auth/Landing";
 import RegisterScreen from "./components/auth/Register";
 import LoginScreen from "./components/auth/Login";
-import { View, Text, Button } from "react-native";
+import MainScreen from "./components/Main";
+import AddScreen from "./components/main/Add";
+import { View, Text } from "react-native";
 
 const Stack = createStackNavigator();
 
@@ -57,9 +65,13 @@ export default function App() {
   }
 
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text>User is logged in</Text>
-      <Button title="Log Out" onPress={() => firebase.auth().signOut()} />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Main">
+          <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Add" component={AddScreen} options={{ headerShown: true }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   )
 }
