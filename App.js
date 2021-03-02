f3778a431ae38510248bfd41346645bef4359684
Andrew Gray <andrew.aj.gray@gmail.com>
2021-03-02 19:34:06 +1030

    added comment view to navigation stack

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import rootReducer from "./redux/reducers";
 import thunk from "redux-thunk";
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-import firebase from "firebase";
+import firebase from "firebase/app";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -28,6 +28,7 @@ import RegisterScreen from "./components/auth/Register";
 import LoginScreen from "./components/auth/Login";
 import MainScreen from "./components/Main";
 import AddScreen from "./components/main/Add";
+import CommentScreen from "./components/main/Comment";
 import SaveScreen from "./components/main/Save";
 import { View, Text } from "react-native";
 
@@ -71,8 +72,9 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Main">
-          <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="Add" component={AddScreen} options={{ headerShown: true }} />
+          <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name="Add" component={AddScreen} />
+          <Stack.Screen name="Comment" component={CommentScreen} />
           <Stack.Screen name="Save" component={SaveScreen} />
         </Stack.Navigator>
       </NavigationContainer>
